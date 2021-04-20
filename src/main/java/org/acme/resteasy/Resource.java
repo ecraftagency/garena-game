a514c866c7f2790bbcc839edc9e1af61d73d3458
Nguyen Hoang Vu <ecraft.eventagency@gmail.com>
2021-04-20 21:44:06 +0700

    minor

--- src/main/java/org/acme/resteasy/Resource.java
+++ src/main/java/org/acme/resteasy/Resource.java
@@ -1,5 +1,7 @@
 package org.acme.resteasy;
 
+import com.zen.kv.AsyncKVService;
+import com.zen.kv.SimpleKVService;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.reactive.RedisReactiveCommands;
@@ -7,8 +9,6 @@ import io.quarkus.runtime.StartupEvent;
 import io.quarkus.vertx.web.Route;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.RoutingContext;
-import org.acme.resteasy.kv.AsyncKVService;
-import org.acme.resteasy.kv.EtcdKvImpl;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -35,7 +35,7 @@ public class Resource {
   String                                redisConn;
   RedisClient                           redisClient;
   RedisReactiveCommands<String, String> rxCommand;
-  AsyncKVService                        kvService;
+  AsyncKVService<String>                kvService;
 
   //todo use cache to mitigate etcd read operation
   //todo key value pair must be tiny so the cache remain under 100MB with 100000 index
@@ -47,37 +47,89 @@ public class Resource {
     redisClient = RedisClient.create(redisConn);
     StatefulRedisConnection<String, String> connection = redisClient.connect();
     rxCommand = connection.reactive();
-    kvService = new EtcdKvImpl(etcdHost);
+    kvService = new SimpleKVService(etcdHost);
     cache = new ConcurrentHashMap<>();
   }
 
   @Route(methods = HttpMethod.GET, regex = ".*/newgame")
   void newGame(RoutingContext ctx) {
-    String id     = ctx.request().getParam("id");
-    String token  = ctx.request().getParam("token");
-
-    String dbToken = cache.get(id);
-    //todo cacheMiss
-    if (dbToken == null) {
-      kvService.get(id, ar -> {
-        if (ar.succeeded() && ar.result() != null && ar.result().equals(token)) {
-          processNewGame(ctx, id);
-          cache.put(id, ar.result());
-          if (cache.size() > maxCacheSize)
-            cache.remove(lastCache);
-          lastCache = ar.result();
-        }
-        else {
-          ctx.response().setStatusCode(404).end();
-        }
-      });
+    String id       = ctx.request().getParam("id");
+    String token    = ctx.request().getParam("token");
+    String dbToken  = cache.get(id);
+    //todo cache hit
+    if (dbToken != null && dbToken.equals(token)) {
+      processNewGame(ctx, id);
     }
     else {
-      if (dbToken.equals(token))
-        processNewGame(ctx, id);
-      else
-        ctx.response().setStatusCode(404).end();
+      kvService.get(id)
+        .doOnSuccess(resp -> {
+          if (resp.equals(token)) {
+            processNewGame(ctx, id);
+            cache.put(id, resp);
+            if (cache.size() > maxCacheSize)
+              cache.remove(lastCache);
+            lastCache = resp;
+          }
+        })
+        .doOnError(err -> ctx.response().setStatusCode(401).end()).subscribe();
     }
+  }
+
+  @Route(methods = HttpMethod.GET, regex = ".*/data")
+  void data(RoutingContext ctx) {
+    String id       = ctx.request().getParam("id");
+    String token    = ctx.request().getParam("token");
+    String dbToken  = cache.get(id);
+
+    if (dbToken != null && dbToken.equals(token)) {
+      processGetUserInfo(ctx, id);
+    }
+    else {
+      kvService.get(id)
+        .doOnSuccess(resp -> {
+          if (resp.equals(token)) {
+            processGetUserInfo(ctx, id);
+            cache.put(id, resp);
+            if (cache.size() > maxCacheSize)
+              cache.remove(lastCache);
+            lastCache = resp;
+          }
+        })
+        .doOnError(err -> ctx.response().setStatusCode(401).end()).subscribe();
+    }
+  }
+
+  void processGetUserInfo(RoutingContext ctx, String id) {
+    long curMs    = System.currentTimeMillis();
+    String def    = 3 + ":" + curMs + ":" + 0;
+
+    rxCommand.get("turn:" + id)
+            .doOnSuccess(s -> {
+              try {
+                String[] turnInfo = s.split(":");
+                if (turnInfo.length != 3)
+                  throw new Exception("corrupt data");
+
+                int turn          = Integer.parseInt(turnInfo[0]);
+                long lastRequest  = Long.parseLong(turnInfo[1]);
+                long score        = Long.parseLong(turnInfo[2]);
+                if (curMs - lastRequest > 60*1000) {
+                  turn = 3;
+                  lastRequest = curMs;
+                }
+                String resp = turn + ":" + lastRequest + ":" + score;
+                rxCommand.set("turn:" + id, resp).subscribe();
+                ctx.response().end(resp);
+              }
+              catch (Exception e) {
+                rxCommand.set("turn:" + id, def).subscribe();
+                ctx.response().end(def);
+              }
+            })
+            .doOnError(err -> {
+              rxCommand.set("turn:" + id, def).subscribe();
+              ctx.response().end(def);
+            }).subscribe();
   }
 
   void processNewGame(RoutingContext ctx, String id) {
@@ -86,35 +138,36 @@ public class Resource {
             .doOnSuccess(s -> {
               try {
                 String[] turnInfo = s.split(":");
-                if (turnInfo.length != 2)
+                if (turnInfo.length != 3)
                   throw new Exception("corrupt data");
 
-                int turn = Integer.parseInt(turnInfo[0]);
-                long lastRequest = Long.parseLong(turnInfo[1]);
+                int turn          = Integer.parseInt(turnInfo[0]);
+                long lastRequest  = Long.parseLong(turnInfo[1]);
+                long score        = Long.parseLong(turnInfo[2]);
                 if (curMs - lastRequest > 60*1000)
-                  updateTurn(ctx, curMs, id, dailyTurn);
+                  updateTurn(ctx, curMs, id, dailyTurn, score);
                 else {
                   if (turn > 0)
-                    updateTurn(ctx, curMs, id, turn);
+                    updateTurn(ctx, curMs, id, turn, score);
                   else
                     ctx.response().end("oom");
                 }
               }
               catch (Exception e) {
-                updateTurn(ctx, curMs, id, dailyTurn);
+                updateTurn(ctx, curMs, id, dailyTurn, 0);
               }
             })
             .doOnError(err -> { //todo don't have turn:id key
-              updateTurn(ctx, curMs, id, dailyTurn);
+              updateTurn(ctx, curMs, id, dailyTurn, 0);
             }).subscribe();
   }
 
-  void updateTurn(RoutingContext ctx, long curMs, String id, int turn) {
+  void updateTurn(RoutingContext ctx, long curMs, String id, int turn, long score) {
     turn--;
     String randSession = rand(10);
     rxCommand.set("session:" + id, randSession).subscribe();
-    rxCommand.set("turn:" + id, turn + ":" + curMs).subscribe();
-    ctx.response().end(randSession);
+    rxCommand.set("turn:" + id, turn + ":" + curMs + ":" + score).subscribe();
+    ctx.response().end(randSession + ":" + turn);
   }
 
   public static String rand(int len) {
